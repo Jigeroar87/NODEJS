5d653e9a7478ae2e523d9f346bfed41e941785f3
Jigeroar87 <jimmy.robles.a@gmail.com>
2023-05-04 10:53:29 -0500

    Componentes II

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
 import './App.css';
+import Card from './Components/Card/Card';
 import Contador from './Components/Contador/Contador';
 //import ItemListContainer from './Components/ItemListContainer/ItemListContainer';
 //import Navbar from './Components/Navbar/Navbar';
 import Container from './Layout/Container';
 import Contacto from './Pages/Contacto';
 import Home from './Pages/Home';
+import {AiFillAndroid} from "react-icons/ai"
 
 function App() {
 
+  const contenido= [
+
+    {
+      titulo: 'batman',
+      descripcion: 'DC'
+    },
+    {
+      titulo: 'superman',
+      descripcion: 'marvel',
+      imagen: 'https://phantom-elmundo.unidadeditorial.es/606b289a9a8bee7fa0dc2a92bc739063/crop/32x0/686x436/resize/600/f/webp/assets/multimedia/imagenes/2022/12/15/16710955260924.png'
+    }
+  ]
+
   return (
     <div className="App">
+
+      <h1><AiFillAndroid/> </h1>
+
       <Container>
         <Home/>
       </Container>  
@@ -22,9 +40,17 @@ function App() {
 
       <h1>_________________________</h1>
 
+      <Contador initialValue={10}/>
       <Contador/>
-      <Contador/>
-      <Contador/>
+      <Contador initialValue={5}/>
+
+    {contenido.map((superheroe,index)=>(
+      <Card key={index} titulo={superheroe.titulo} descripcion={superheroe.descripcion} imagen={superheroe.imagen}/>
+    )
+
+    )}
+
+    
 
     </div>
 
--- src/Components/Contador/Contador.js
+++ src/Components/Contador/Contador.js
@@ -1,6 +1,14 @@
-import {useState} from 'react'
+import {useState, useEffect, useRef} from 'react'
 
-const Contador = () => {
+const Contador = (props) => {
+    
+    //Ciclo de vida
+    //Mount
+    //Change
+    //Dismount
+
+    //Mount(0)=> Change=> Dismount => Mount(1) ? 
+    
     // Hook=> son métodos que nos provee React para solucionar cosas
     //PD => todos los hooks arrancan con la palabra "use"
     //1. state 
@@ -8,7 +16,36 @@ const Contador = () => {
     // > el valor y la funcion que va a cambiar el valor
     // > cual va a ser su valor inicial
 
-    const [numero, setNumero] = useState(0)
+    const{initialValue} = props
+
+    const [numero, setNumero] = useState(initialValue||0)
+    //const [nombre, setNombre] = useState('')
+    //const [darkmode, setDarmode] = useState(false)
+    const [limitado, setLimitado] = useState(false)
+
+    const titleDivRef = useRef('')
+
+    const handleTitleAdd=()=>{
+        titleDivRef.current.innerHTML='Nuevo Titulo'
+    }
+
+    useEffect(() => {
+      //first //=>Mount
+        console.log('Se consulto el valor del numero')
+        if (numero>=10 ) {
+            setLimitado(true)
+        }
+
+      return () => {
+        //second // => Dismount
+        setLimitado(false)
+        //setNumero(0)
+      }
+    }, [
+        //third // Cambios
+        numero
+    ])
+    
 
     const sumar = () => {
         setNumero(numero + 1)
@@ -22,13 +59,15 @@ const Contador = () => {
     }
 
   return (
-    <div className='d-flex justify-around'>
+    <div className={ ` ${limitado ? 'bg-danger':''} 'd-flex justify-around' `}  >
+        <div ref={titleDivRef}> </div>
+        <button onClick={handleTitleAdd} >Agregar un titulo</button>
         <div>
             <button onClick={restar} >Restar</button>
         </div>
             <h3 className='text-center'>Numero actual es: {numero}</h3>
         <div>
-            <button onClick={sumar}>Sumar</button>
+            <button disabled={limitado} onClick={sumar}>Sumar</button>
         </div>
         <div>
             <button onClick={resetear}>Resetar</button>
