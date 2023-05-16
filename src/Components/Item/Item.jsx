91865a87bbfc35ea193bf01de9505c5a4714a752
Jigeroar87 <jimmy.robles.a@gmail.com>
2023-05-16 18:00:05 -0500

    Avance 2dodesafio todo sin makeup

--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import {useNavigate} from 'react-router-dom'
 
 const Item = (props) => {
-  const {id,titulo,descripcion} = props;
+  const {id,titulo,descripcion,imagen} = props;
   const navigate = useNavigate()
   return (
     <div className='d-flex flex-column w-25 h-25 border border-dark mx-2'>
         <h3 className='fs-5'>{titulo}</h3>
+        <img src={imagen} className="card-img-top" alt="..."/>
         <p className='fs-6'>{descripcion}</p>
         {/* <Link to={`/item/${id}`} className='btn btn-warning' >Ver Mas</Link> */}
         <button onClick={()=>navigate(`/Productos/${id}`)}>Ver mas</button>
@@ -15,3 +16,5 @@ const Item = (props) => {
 }
 
 export default Item
+
+
